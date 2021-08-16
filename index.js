b58feffa94494dca2acf28f37eac49dfed75132f
Zachpaulino7 <Zachp7@live.com>
2021-08-16 01:56:40 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let employee = {
-
+streetAddress: '',
+name: '',
 }
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return Object.assign({}, employee, { [key]: value })
+    // let newEmployee = {...employee}
+    // newEmployee[key] = value
+    // return newEmployee
+    return Object.assign({}, employee, {[key]: value})
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // employee[key] = value
+    // return employee
     return Object.assign(employee, { [key]: value })
 }
 
@@ -14,6 +20,7 @@ function deleteFromEmployeeByKey(employee, key) {
     const newEmployee = Object.assign({}, employee)
     delete newEmployee[key]
     return newEmployee
+    
 }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
